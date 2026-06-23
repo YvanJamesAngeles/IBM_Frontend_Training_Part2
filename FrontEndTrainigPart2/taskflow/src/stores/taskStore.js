e50b29946b4a98c91e10f9949ff2396421259f70
Yvan James Angeles <yvan.james.angeles@ibm.com>
2026-06-23 10:38:22 +0800

    initial commit

---
+++ FrontEndTrainigPart2/taskflow/src/stores/taskStore.js
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('tasks', () => {
+  const tasks  = ref([])
+  const nextId = ref(1)
+
+  const totalCount   = computed(() =>  tasks.value.length)
+  const doneCount    = computed(() =>  tasks.value.filter(task => task.done).length)
+  const pendingCount = computed(() => tasks.value.filter(task => !task.done).length)
+
+  function addTask(name) {
+    if (!name || name.trim() === '') return
+    tasks.value.push({
+      id: nextId.value++,
+      name,
+      done: false
+    })
+  }
+
+  function toggleTask(id) {
+    const task = tasks.value.find(task => task.id === id);
+
+    if(task){
+        task.done = !task.done
+    }
+  }
+
+  function removeTask(id) {
+    tasks.value = tasks.value.filter(task => task.id !== id);
+  }
+
+   return {
+    tasks,
+    nextId,
+    totalCount,
+    doneCount,
+    pendingCount,
+    addTask,
+    toggleTask,
+    removeTask
+  }
+})
